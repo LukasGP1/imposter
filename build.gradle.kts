7d02252126e77d5dc2209563e10bc745c781b22d
LukasGP1 <lukas.hilger@icloud.com>
2025-07-08 15:47:57 +0200

    made gradle work

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,34 @@
 plugins {
     id("java")
+    id("application")
 }
 
 group = "de.lulkas_"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+
 }
 
-tasks.test {
-    useJUnitPlatform()
+application {
+    mainClass.set("de.lulkas_.Main")
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "de.lulkas_.Main"
+    }
+
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath.get())
+    from({
+        configurations.runtimeClasspath.get()
+            .filter { it.name.endsWith("jar") }
+            .map { zipTree(it) }
+    })
 }
